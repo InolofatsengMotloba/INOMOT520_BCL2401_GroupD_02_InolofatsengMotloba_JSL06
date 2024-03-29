c92d81b286e65e133941cba7aab7ade854b70b77
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-03-29 13:39:32 +0200

    Function to display menu items by category

--- index.js
+++ index.js
@@ -8,36 +8,37 @@ const menu = {
 // Function to display menu items by category
 function displayMenuItems(menu) {
     // Get the menu container element from the HTML
+    const menuContainerElement = document.getElementById("menu");
 
     // Loop through each category and its items in the menu object
-
+    for (let [category, items] of Object.entries(menu)) {
         // Create an element to represent the category
-
+        const categoryElement = document.createElement("h2");
         // Set the text content of the category element to the category name
-
+        categoryElement.textContent = category;
         // Append the category element to the menu container
-
+        menuContainerElement.appendChild(categoryElement);
         // Create an element to represent a list of items
-
+        const listItemElement = document.createElement("ul");
         // Append a list of items element to the menu container
-
+        menuContainerElement.append(listItemElement)
         // Loop through the items in the category and create list items
-
+        items.forEach(item => {
             // Create a list item element
-
+            const listItem = document.createElement("li");
             // Set the text content of the list item element to the item name
-
+            listItem.textContent = item;
             // Attach a click event listener to the list item to add it to the order
-
+            listItem.addEventListener("click", () => addToOrder(item));
             // Append the list item to the list of items
-
-            
+            listItemElement.appendChild(listItem);
+        });
+    }          
 }
 
 // Callback function for adding an item to the order
 function addToOrder(itemName) {
     // Get the order items list and the order total element from the HTML
-
     // Create a list item for the order
 
     // Set the text content of the list item to the item name
